675a6220804176a7938839fb5f9feb195a42afc7
Fabio Ventura <fabio.ventura@muoversi.net>
2016-01-09 17:24:28 +0100

    removed useless comments

--- module/Accantona/src/Accantona/Controller/AccantonatoController.php
+++ module/Accantona/src/Accantona/Controller/AccantonatoController.php
@@ -15,24 +15,14 @@ class AccantonatoController extends AbstractActionController
 
     public function indexAction()
     {
-//        $sm = $this->getServiceLocator();
-//        $categoryTable = $sm->get('Accantona\Model\CategoriaTable');
-//
 //        $where = array();
-//
-//        if (($categoryId = (int)$this->params()->fromQuery('categoryIdFilter', 0)) != false) {
-//            $where[] = "categorie.id=$categoryId";
-//        }
         if (($months = (int)$this->params()->fromQuery('monthsFilter', 1)) != false) {
-//            $where[] = 'spese.valuta>"' . date('Y-m-d', strtotime("-$months month")) . '"';
+            $where[] = 'spese.valuta>"' . date('Y-m-d', strtotime("-$months month")) . '"';
         }
 //
         return new ViewModel(array(
-//            'categoryId' => $categoryId,
             'months' => $months,
 //            'rows' => $this->getSpesaTable()->joinFetchAll($where),
-//            'categories' => $categoryTable->fetchAll(array(), 'descrizione')->toArray(),
-//            'avgPerCategory' => $this->getSpesaTable()->getAvgPerCategories(),
         ));
     }
 
