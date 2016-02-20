fb2463b40c74c9988381fffe27c8ad350d831139
Fabio Ventura <foravatenubi@gmail.com>
2016-02-21 00:08:02 +0100

    #3 new module auth

--- config/application.config.php
+++ config/application.config.php
@@ -4,6 +4,7 @@ return array(
     'modules' => array(
         'Accantona',
         'Application',
+        'Auth',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
