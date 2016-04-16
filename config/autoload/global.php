39bac6d6aae1a470dc9fbbdb44c081cc1f42c760
Fabio Ventura <foravatenubi@gmail.com>
2016-04-16 19:26:11 +0200

    modified config files

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,34 +12,17 @@
  */
 
 // set main configurations
-$conf = array(
+return array(
     'db' => array(
         'driver' => 'Pdo',
-        'dsn' => 'mysql:dbname=accantona_prod;host=localhost',
-        'driver_options' => array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''),
-        'username' => 'root',
-        'password' => 'root',
+//        'dsn' => 'mysql:dbname=accantona-prod;host=localhost',
+//        'driver_options' => array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''),
+//        'username' => 'root',
+//        'password' => 'root',
 //        'driver'   => 'Pdo_Sqlite',
 //        'database' => __DIR__ . '/../../data/accantona.sqlite',
     ),
     'service_manager' => array(
         'factories' => array('Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory'),
     ),
-    
-    'static_salt' => 'pippo',
 );
-
-// set configuration for specific environment
-switch (APP_ENV) {
-    case 'development':
-        $conf['db'] = array(
-            'driver' => 'Pdo',
-            'dsn' => 'mysql:dbname=accantona_dev;host=localhost',
-            'driver_options' => array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''),
-            'username' => 'root',
-            'password' => 'root',
-        );
-        break;
-}
-
-return $conf;
