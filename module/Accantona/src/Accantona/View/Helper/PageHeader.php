8371a92d442cea82a28086e6c62bb0303d8f0915
Fabio Ventura <foravatenubi@gmail.com>
2016-08-29 23:45:58 +0200

    fix view

--- module/Accantona/src/Accantona/View/Helper/PageHeader.php
+++ module/Accantona/src/Accantona/View/Helper/PageHeader.php
@@ -2,7 +2,6 @@
 namespace Accantona\View\Helper;
 
 // use \Zend\I18n\View\Helper\DateFormat;
-use Zend\Debug\Debug;
 use Zend\View\Helper\AbstractHelper;
 
 class PageHeader extends AbstractHelper
@@ -10,6 +9,7 @@ class PageHeader extends AbstractHelper
     public function __invoke($title)
     {
         $str = $this->view->escapeHtml($title);
+        $this->view->headTitle($str);
         return "<div class=\"row\"><div class=\"col-lg-12\"><h1 class=\"page-header\">$str</h1></div></div>";
     }
 }
