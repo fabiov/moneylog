176c6d78b55376fc8f79fdb54ea25d92d2106daa
Fabio Ventura <foravatenubi@gmail.com>
2018-01-23 21:36:23 +0100

    #11 add Auth adapter

---
+++ module/Auth/src/Auth/Service/AuthAdapter.php
@@ -0,0 +1,93 @@
+<?php
+namespace Auth\Service;
+
+use Application\Entity\User;
+use Zend\Authentication\Adapter\AdapterInterface;
+use Zend\Authentication\Result;
+
+/**
+ * Adapter used for authenticating user. It takes login and password on input
+ * and checks the database if there is a user with such login (email) and password.
+ * If such user exists, the service returns his identity (email).
+ * The identity is saved to session and can be retrieved later with Identity view helper providedby ZF3.
+ */
+class AuthAdapter implements AdapterInterface
+{
+    /**
+     * User email.
+     * @var string
+     */
+    private $email;
+
+    /**
+     * Password
+     * @var string
+     */
+    private $password;
+
+    /**
+     * Entity manager.
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $entityManager;
+
+    /**
+     * AuthAdapter constructor.
+     * @param $entityManager
+     */
+    public function __construct($entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * Sets user email.
+     *
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Sets password.
+     *
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = (string) $password;
+    }
+
+    /**
+     * Performs an authentication attempt.
+     */
+    public function authenticate(): Result
+    {
+        /* @var \Application\Entity\User $user */
+        // Check the database if there is a user with such email.
+        $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $this->email]);
+
+        // If there is no such user, return 'Identity Not Found' status.
+        if (!$user) {
+            return new Result(Result::FAILURE_IDENTITY_NOT_FOUND, null, ['Invalid credentials.']);
+        }
+
+        // If the user with such email exists, we need to check if it is confirmed.
+        // Do not allow not confimed users to log in.
+        if ($user->getStatus() == User::STATUS_NOT_CONFIRMED) {
+            return new Result(Result::FAILURE, null, ['User not confirmed.']);
+        }
+
+        // Now we need to calculate hash based on user-entered password and compare
+        // it with the password hash stored in database.
+        if ($user->getPassword() === md5($this->password . $user->getSalt())) {
+            // Great! The password hash matches. Return user identity (email) to be saved in session for later use.
+            return new Result(Result::SUCCESS, $this->email, ['Authenticated successfully.']);
+        }
+
+        // If password check didn't pass return 'Invalid Credential' failure status.
+        return new Result(Result::FAILURE_CREDENTIAL_INVALID, null, ['Invalid credentials.']);
+    }
+}
