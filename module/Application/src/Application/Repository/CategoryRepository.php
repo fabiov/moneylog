57150c5ccc9905d73c80cc4df31317e67d9faeec
Fabio Ventura <foravatenubi@gmail.com>
2019-02-21 23:19:47 +0100

    #91 add function for oldest movements for category

--- module/Application/src/Application/Repository/CategoryRepository.php
+++ module/Application/src/Application/Repository/CategoryRepository.php
@@ -1,9 +1,9 @@
 <?php
-
 namespace Application\Repository;
 
+use Application\Entity\Category;
+use Application\Entity\Movement;
 use Doctrine\ORM\EntityRepository;
-use Zend\Debug\Debug;
 
 class CategoryRepository extends EntityRepository
 {
@@ -20,16 +20,12 @@ class CategoryRepository extends EntityRepository
         $qb = $this->getEntityManager()
             ->createQueryBuilder()
             ->select('SUM(m.amount) AS amount, MIN(m.date) AS first_date, c.id, c.descrizione')
-            ->from('Application\Entity\Movement', 'm')
+            ->from(Movement::class, 'm')
             ->innerJoin('m.category', 'c')
             ->where('c.userId=:userId')
             ->andWhere('c.status=:status')
             ->andWhere('m.date >= :since')
-            ->setParameters([
-                ':since'  => $since->format('Y-m-d'),
-                ':status' => 1,
-                ':userId' => $userId,
-            ])
+            ->setParameters([':since'  => $since->format('Y-m-d'), ':status' => 1, ':userId' => $userId])
             ->groupBy('c.id');
 
         $rs = $qb->getQuery()->getResult();
@@ -46,5 +42,27 @@ class CategoryRepository extends EntityRepository
 
         }
         return $data;
+    }
+    
+    /**
+     * Get the averages for categories
+     *
+     * @param int $userId
+     * @param int $status null for all statuses
+     * @return array
+     */
+    public function oldestMovements(int $userId, int $status = null)
+    {
+        $qb = $this->getEntityManager()
+            ->createQueryBuilder()
+            ->select('c.id, c.descrizione, MIN(m.date) AS oldest, c.status')
+            ->from(Category::class, 'c')
+            ->leftJoin(Movement::class, 'm', 'WITH', 'c.id=m.category')
+            ->where("c.userId=$userId")
+            ->groupBy('c.id');
+        if ($status !== null) {
+            $qb->andWhere("c.status=$status");
+        }
+        return $qb->getQuery()->getResult();
     }
 }
