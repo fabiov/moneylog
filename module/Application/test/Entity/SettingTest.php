58120a44624a0a8917b7ef74dc314849c1691861
Fabio Ventura <foravatenubi@gmail.com>
2021-05-23 20:56:40 +0200

    #147 add Setting tests

---
+++ module/Application/test/Entity/SettingTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace ApplicationTest\Entity;
+
+use Application\Entity\Setting;
+use Application\Entity\User;
+use Laminas\InputFilter\InputFilter;
+use PHPUnit\Framework\TestCase;
+
+class SettingTest extends TestCase
+{
+    public function testGettersAndSetters(): void
+    {
+        $user = new User();
+        $setting = new Setting($user);
+
+        $months = 12;
+        $setting->setMonths($months);
+        self::assertSame($months, $setting->getMonths());
+
+        $payday = 2;
+        $setting->setPayday($payday);
+        self::assertSame($payday, $setting->getPayday());
+
+        $provisioning = true;
+        $setting->setProvisioning($provisioning);
+        self::assertSame($provisioning, $setting->hasProvisioning());
+
+        self::assertInstanceOf(InputFilter::class, $setting->getInputFilter());
+
+        self::expectException(\Exception::class);
+        $setting->setInputFilter(new InputFilter());
+    }
+
+    public function setPaydayException(): void
+    {
+        $user = new User();
+        $setting = new Setting($user);
+
+        self::expectException(\Exception::class);
+        $setting->setPayday(29);
+    }
+
+    public function testArrayExchangeAndCopy(): void
+    {
+        $user = new User();
+        $setting = new Setting($user);
+
+        $months = 54;
+        $payday = 12;
+        $provisioning = true;
+
+        $setting->exchangeArray([
+            'months' => $months,
+            'payday' => $payday,
+            'provisioning' => $provisioning,
+        ]);
+
+        $copy = $setting->getArrayCopy();
+
+        self::assertSame($months, $copy['months']);
+        self::assertSame($payday, $copy['payday']);
+        self::assertSame($provisioning, $copy['provisioning']);
+    }
+}
